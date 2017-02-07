93fb43853f107fd502a399a33c55e4a248f95cd7
raz <bogdrazvan@gmail.com>
2017-02-08 00:13:47 +0200

    refactoring

--- source/js/App.js
+++ source/js/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import CurrentDate from "./CurrentDate.js";
 import CurrentTime from "./CurrentTime.js";
 import CurrentTimezone from "./CurrentTimezone.js";
+import moment from 'moment';
 
 class App extends React.Component {
 
@@ -10,26 +11,28 @@ class App extends React.Component {
     super();
 
     this.state = {
-      status: ''
+      curTime: undefined
     };
+
+    this.updateCurrentTime = this.updateCurrentTime.bind(this);
   }
-// work here
+
   componentDidMount() {
-    this.setState({
-      status: <div>
-        <CurrentDate />
-        <CurrentTime />
-        <CurrentTimezone />
-      </div>
-    });
+    setInterval(() => this.updateCurrentTime()
+    , 1000);
+  }
+
+  updateCurrentTime() {
+      this.setState({
+        curTime: moment().format("HH:mm:ss")
+      });
   }
 
   render() {
-    let status = this.state.status;
     return (
       <div>
         <CurrentDate />
-        <CurrentTime />
+        <CurrentTime curTime={this.state.curTime}/>
         <CurrentTimezone />
       </div>
     );
--- source/js/CurrentTime.js
+++ source/js/CurrentTime.js
@@ -4,31 +4,14 @@ import moment from 'moment';
 
 class CurrentTime extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      curTime: undefined
-    }
-
-    this.updateCurrentTime = this.updateCurrentTime.bind(this);
-  }
-
-  componentDidMount() {
-    setInterval(() => this.updateCurrentTime()
-    , 1000);
-  }
-
-  updateCurrentTime() {
-      this.setState({
-        curTime: moment().format("HH:mm:ss")
-      });
+  constructor(props) {
+    super(props);
   }
 
   render() {
     let time;
-
-    if (this.state.curTime) {
-          time = `Your current time is: ${this.state.curTime}`;
+    if (this.props && this.props.curTime) {
+          time = `Your current time is: ${this.props.curTime}`;
     }
 
     return <h3>{time}</h3>
